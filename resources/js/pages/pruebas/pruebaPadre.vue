d5bbdc0f3be341925731ded2a8fa28afee4e5597
dyn <dayana26ce@gmail.com>
2023-02-13 15:41:39 -0500

    t

--- resources/js/pages/pruebas/pruebaPadre.vue
+++ resources/js/pages/pruebas/pruebaPadre.vue
@@ -1,129 +1,173 @@
-<template>
-  <v-card flat tile>
-    <v-container class="white" fluid>
-      <v-subheader class="spacing-playground py-12 d-flex justify-space-between">
-        <v-row class="d-flex align-center">
-          <v-img
-            lazy-src="https://picsum.photos/id/11/10/6"
-            max-height="90"
-            max-width="140"
-            src="https://acortar.link/RqLdLA"
-          >
-          </v-img>
-          <span class="text-h5 mx-1"> Classroom</span>
-        </v-row>
-        <v-img
-          lazy-src="https://picsum.photos/id/11/10/6"
-          max-height="70"
-          max-width="90"
-          src="https://img.freepik.com/vector-premium/icono-perfil-personas_24877-40756.jpg?w=2000"
-        >
-        </v-img>
-      </v-subheader>
-
-      <v-divider></v-divider>
-
-      <v-row class="spacing-playground pa-10">
-        <v-col v-for="(d, c) in cursos" :key="c" cols="12" sm="5" md="3">
-          <v-card>
-            <v-img
-              :src="
-                `https://us.123rf.com/450wm/valex113/valex1131607/valex113160700012/60124041-resumen-3d-de-ondas-con-part%C3%ADculas-en-el-fondo-negro-fondo-futurista-con-las-l%C3%ADneas-de-muchas-esfera.jpg${getImage()}`
-              "
-              height="200"
-              width="300"
-            >
-              <span class="text-h5 white--text pl-4 pt-4 d-inline-block">
-                {{ d.Curso }}
-              </span>
-              <br /><br />
-              <br /><br />
-              <v-row>
-                <v-col cols="12" md="6">
-                  <v-card-text
-                    class="text-h8 white--text pl-4 pt-4 d-inline-block"
-                  >
-                    <strong> Docente:<br /> </strong> {{ d.docente }}
-                  </v-card-text>
-                </v-col>
-                <v-col class="mx-15" cols="12" md="2">
-                  <v-img
-                    lazy-src="https://picsum.photos/id/11/10/6"
-                    max-height="70"
-                    max-width="90"
-                    src="https://img.freepik.com/vector-premium/icono-perfil-personas_24877-40756.jpg?w=2000"
-                    class="justify-end"
-                  >
+<template >
+  <v-card>
+  <div style="background-color: #202225;" class="mt-4" >
+    <v-row  >
+      <v-col cols="12" md="1" >
+         <v-row > 
+          <v-avatar size="60px" class="ml-6 mt-4" >
+           <!-- <v-img v-for="(i) in imagen"
+                  :key="i"
+                  :src="i.imag"></v-img> -->
+                <v-img 
+                    src="https://cdn.vuetifyjs.com/images/john.jpg">
                   </v-img>
-                </v-col>
-              </v-row>
-            </v-img>
+          </v-avatar>
+        </v-row>
 
-            <v-card-actions class="white justify-end">
-              <v-btn
-                v-for="(imgico, i) in icono"
-                :key="i"
-                :color="imgico.color"
-                class="white--text"
-                fab
-                icon
-                small
-              >
-                <v-icon>{{ imgico.icon }}</v-icon>
-              </v-btn>
-            </v-card-actions>
-          </v-card>
-        </v-col>
-      </v-row>
-    </v-container>
+        <v-row > 
+          <v-avatar size="60px" class="ml-6 mt-4" >
+                <v-img 
+                    src="https://i.pinimg.com/736x/b5/49/41/b5494197b2d462c940f88988b203d290.jpg">
+                  </v-img>
+          </v-avatar>
+        </v-row>
+
+        <v-row > 
+          <v-avatar size="60px" class="ml-6 mt-4" >
+                <v-img 
+                    src="https://d500.epimg.net/cincodias/imagenes/2016/07/04/lifestyle/1467646262_522853_1467646344_noticia_normal.jpg">
+                  </v-img>
+          </v-avatar>
+        </v-row>
+
+        <v-row > 
+          <v-avatar size="60px" class="ml-6 mt-4" >
+                <v-img 
+                    src="https://cdn.vuetifyjs.com/images/john.jpg">
+                  </v-img>
+          </v-avatar>
+        </v-row>
+
+        <v-row > 
+          <v-avatar size="60px" class="ml-6 mt-4" >
+                <v-img 
+                    src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS_0Fi4Paya8VLXML0EFyoIy0M9V8zAB2LMqg&usqp=CAU">
+                  </v-img>
+          </v-avatar>
+        </v-row>
+
+        <v-row > 
+          <v-avatar size="60px" class="ml-6 mt-4" >
+                <v-img 
+                    src="https://i.pinimg.com/736x/b5/49/41/b5494197b2d462c940f88988b203d290.jpg">
+                  </v-img>
+          </v-avatar>
+        </v-row>
+
+        <v-row > 
+          <v-avatar size="60px" class="ml-6 mt-4" >
+                <v-img 
+                    src="https://d500.epimg.net/cincodias/imagenes/2016/07/04/lifestyle/1467646262_522853_1467646344_noticia_normal.jpg">
+                  </v-img>
+          </v-avatar>
+        </v-row>
+
+
+      
+      </v-col>
+      
+      <v-col cols="12" md="2" style="background-color:#2f3136; te">
+        <v-row class="ml-3 my-3" >
+          <span class="white--text"> Equipo de Desarrollo</span>
+        </v-row>
+
+        <v-row class="ml-3 my-3" >
+            <span class="white--text "> MUSICAS <br> </span>  
+             <span v-for="(c) in MUSICAS"  :key="c" class="text-h7 white--text d-inline-block "> <br>  # {{ c.d }}</span>         
+          </v-row>
+
+          <v-row class="ml-3 my-3" >
+            <span class="white--text "> GENERAL</span> 
+            <br><span v-for="(c) in GENERAL"  :key="c" class="text-h7 white--text d-inline-block "> <br>  # {{ c.d }}</span>         
+          
+          </v-row>
+
+          <v-row class="ml-3 my-3" >
+            <span class="white--text "> GRADOS Y TITULOS</span> 
+            <br><span v-for="(c) in GRADOS"  :key="c" class="text-h7 white--text d-inline-block ">   # {{ c.d }} <br></span>         
+          
+          </v-row>
+
+          <v-row class="ml-3 my-3" >
+            <span class="white--text "> PRACTICAS</span> 
+            <br><span v-for="(c) in PRACTICAS"  :key="c" class="text-h7 white--text d-inline-block "> <br>  # {{ c.d }}</span>         
+          
+          </v-row>
+        
+      </v-col>
+
+      <v-col cols="12" md="9" style="background-color: #000000;" >
+        <v-row class=" d-flex justify-center mx-10 my-10">
+            <v-col class=" d-flex justify-center mr-1 " cols="12" md="5" style="background-color: #18191c;" >
+              <v-avatar class=" ml-6 my-10" size="90px" >
+                <v-img 
+                    src="https://img.freepik.com/vector-premium/icono-perfil-personas_24877-40756.jpg?w=2000">
+                  </v-img>
+              </v-avatar>                
+            </v-col>
+
+            <v-col class=" d-flex justify-center ml-1" cols="12" md="5" style="background-color: #893143;">
+              <v-avatar class=" ml-6 my-10" size="90px" >
+                <v-img 
+                    src="https://i.pinimg.com/736x/b5/49/41/b5494197b2d462c940f88988b203d290.jpg">
+                  </v-img>
+              </v-avatar>  
+            </v-col>
+        </v-row>
+
+        <v-row class=" d-flex justify-center mx-10 my-10" >
+            <v-col class=" d-flex justify-center 12 mr-1"  cols="12" md="5" style="background-color: #594a61;">
+              <v-avatar class=" ml-6 my-10" size="90px" >
+                <v-img 
+                    src="https://d500.epimg.net/cincodias/imagenes/2016/07/04/lifestyle/1467646262_522853_1467646344_noticia_normal.jpg">
+                  </v-img>
+              </v-avatar>
+            </v-col>
+            <v-col class=" d-flex justify-center 12 ml-1" md="5" style="background-color: #beaea0;">
+              <v-avatar class=" ml-6 my-10" size="90px" >
+                <v-img 
+                    src="https://acortar.link/sAvrMX">
+                  </v-img>
+              </v-avatar>
+            
+            </v-col>
+        </v-row>
+        
+      </v-col>
+
+    </v-row>
+  </div>
+
   </v-card>
 </template>
 
+
 <script>
 export default {
-  data: () => ({
-    cursos: [
-      {
-        Curso: "PROGRAMCION",
-        docente: "Ing. RAMIREZ",
-        Imag: ""
-      },
+  data () {
+    return{
+      imagen: [
+        {
+          imag: "https://cdn.vuetifyjs.com/images/john.jpg"
+        },
+        {
+          imag: "https://acortar.link/PnX0https://i.pinimg.com/736x/b5/49/41/b5494197b2d462c940f88988b203d290.jpgHC"
+        },
+        {
+          imag: "https://d500.epimg.net/cincodias/imagenes/2016/07/04/lifestyle/1467646262_522853_1467646344_noticia_normal.jpg"
+        }
 
-      {
-        Curso: "SIMULACION",
-        docente: "Ing. NEGRETE",
-        Imag: ""
-      },
+      ],
 
-      {
-        Curso: "TEORIA DE SISTEMAS",
-        docente: "Ing. MUÑOZ",
-        Imag: ""
-      },
+        MUSICAS: [{ d:"musicas"},{d:"music-comands"} ],
+         GENERAL:[ { d:"chat-general"},{d:"Reuniones Generales"}],
+         GRADOS: [{ d:"herramientas"},{d:"tareas"},{d:"chat"},{d:"documentos"},
+                  {d:"extras"},{d:"capturas"},{d:"Reuniones"}],
+         PRACTICAS:[ { d:"tarea"}]
 
-      {
-        Curso: "ADMINITRACION",
-        docente: "Ing. Sosa",
-        Imag: ""
-      }
-    ],
-    icono: [
-      {
-        icon: "mdi-email",
-        color: "blue-grey darken-2"
-      },
-      {
-        icon: "mdi-arrow-up-bold-box-outline",
-        color: "blue-grey darken-2"
-      }
-    ]
-  }),
-  methods: {
-    getImage() {
-      const min = 550;
-      const max = 560;
-      return Math.floor(Math.random() * (max - min + 1)) + min;
-    }
+    
   }
+}
+
 };
 </script>
