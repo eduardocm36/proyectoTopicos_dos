d0e4e513c236dd1692c840b694daaafa9aa7c279
Eduardo Cruz Matias <ecruzmatias5@gmail.com>
2023-06-20 16:38:16 -0500

    inicio

--- resources/js/components/Navbar.vue
+++ resources/js/components/Navbar.vue
@@ -3,7 +3,7 @@
 
     <v-toolbar dense elevation="0" style="    background-color: transparent;  color:#fff;">
      <!--v-img class="ml-10 mr-5" max-height="38" max-width="88" src="/img/cbmediclogo.png" /-->
-      <v-toolbar-title >{{ appName }}</v-toolbar-title>
+      <v-toolbar-title class="ml-7">{{ appName }}</v-toolbar-title>
       <v-spacer></v-spacer>
 
       <v-btn v-for="(link, i) in linksVerified" :key="i"  plain :to="link.path" color="#fff"  >
--- resources/js/pages/admin/index.vue
+++ resources/js/pages/admin/index.vue
@@ -1,5 +1,6 @@
 <template>
-   <div>   
+   <div>
+    <navigation :items="items" :paneles="paneles"/>
      <div>
        <router-view></router-view>
      </div>     
@@ -7,6 +8,9 @@
 </template> 
 
 <script>
+import { mapState } from 'vuex';
+import navigation from "../../components/navigation.vue";
+
 export default {
 
  middleware:["auth", "role:1"],
@@ -14,6 +18,25 @@ export default {
   metaInfo() {
     return { title: this.$t("Administrador") };
   },
+  components: {
+    navigation
+  },
+  data() {
+    return {
+      items: [
+        { title: "Control de Paneles", icon: "mdi-account", to: "dash.admin" },
+        { title: "Usuarios", icon: "mdi-account", to: "users.admin" },
+        { title: "Cursos", icon: "mdi-account", to: "cursos.admin" },
+        { title: "Mis Paneles", icon: "mdi-account", to: "paneles.admin" }
+      ]
+    };
+  },
+  components: {
+    navigation
+  },
+  computed: {
+    ...mapState("admin", ["paneles"]),
+  }
 
 };
 </script>
