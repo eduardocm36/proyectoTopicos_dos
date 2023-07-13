a9f6c1eeb0bf4d1d1e23dfa9305845af943ef836
Oriol_simon <orioltunino1234@gmail.com>
2023-07-13 13:34:47 -0500

    traduccion de email

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,9 @@
 
 namespace App\Providers;
 
+use App\Notifications\VerifyEmail;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Support\Facades\Lang;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 use Laravel\Dusk\DuskServiceProvider;
@@ -18,6 +21,13 @@ class AppServiceProvider extends ServiceProvider
         if ($this->app->runningUnitTests()) {
             Schema::defaultStringLength(191);
         }
+        VerifyEmail::$toMailCallback = function ($notifiable, $verificationUrl){
+            return (new MailMessage)
+            ->subject(Lang::get('Verificación de su cuenta'))
+            ->line(Lang::get('Haga clic en el botón para verificar su dirección de correo electrónico.'))
+            ->action(Lang::get('Confirmar correo electrónico'), $verificationUrl)
+            ->line(Lang::get('Si no creó una cuenta, no se requiere ninguna otra acción.'));
+        };
     }
 
     /**
